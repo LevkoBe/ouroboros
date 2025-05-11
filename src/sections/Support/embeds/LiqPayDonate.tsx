ef3e46e9e010ba5d636ac78f7b3afdf87540b8cc
LevkoBe <lbeniakh@kse.org.ua>
2025-05-12 00:53:09 +0300

    fix attempt 2: liqpay

--- src/sections/Support/embeds/LiqPayDonate.tsx
+++ src/sections/Support/embeds/LiqPayDonate.tsx
@@ -10,28 +10,35 @@ const LiqPayDonate: React.FC<LiqPayDonateProps> = ({ data, signature }) => {
     const script = document.createElement("script");
     script.src = "https://static.liqpay.ua/libjs/checkout.js";
     script.async = true;
-    script.defer = true;
-
     script.onload = () => {
-      // @ts-expect-error: LiqPayCheckoutCallback is not defined on the window object by TypeScript
-      window.LiqPayCheckoutCallback = function () {
-        // @ts-expect-error: LiqPayCheckout is not recognized by TypeScript as it is dynamically loaded
-        LiqPayCheckout.init({
-          data,
-          signature,
-          embedTo: "#liqpay_checkout",
-          mode: "embed",
-        })
-          .on("liqpay.callback", function (data: unknown) {
-            console.log("Payment callback", data);
+      // @ts-expect-error: LiqPayCheckout isn't recognized by Typescript
+      if (window.LiqPayCheckout) {
+        // @ts-expect-error: LiqPayCheckout is not recognized by TypeScript
+        window.LiqPayCheckoutCallback = function () {
+          // @ts-expect-error: LiqPayCheckout is not recognized by TypeScript as it is dynamically loaded
+          LiqPayCheckout.init({
+            data,
+            signature,
+            embedTo: "#liqpay_checkout",
+            mode: "embed",
           })
-          .on("liqpay.ready", function () {
-            console.log("LiqPay widget ready");
-          })
-          .on("liqpay.close", function () {
-            console.log("LiqPay widget closed");
-          });
-      };
+            .on("liqpay.callback", function (data: unknown) {
+              console.log("Payment callback", data);
+            })
+            .on("liqpay.ready", function () {
+              console.log("LiqPay widget ready");
+            })
+            .on("liqpay.close", function () {
+              console.log("LiqPay widget closed");
+            });
+        };
+      } else {
+        console.error("LiqPayCheckout script is not loaded properly.");
+      }
+    };
+
+    script.onerror = () => {
+      console.error("Failed to load LiqPay script.");
     };
 
     document.body.appendChild(script);
